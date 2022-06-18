d13be6540535ff34eaaabcc7d7728236b84754c5
Everett Harris <everettharr979@gmail.com>
2022-06-18 17:40:02 -0400

    background color changed

--- Lotto-greatness/Form1.Designer.cs
+++ Lotto-greatness/Form1.Designer.cs
@@ -131,7 +131,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(379, 170);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(379, 121);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.BtnReset);
             this.Controls.Add(this.BtnPick);
